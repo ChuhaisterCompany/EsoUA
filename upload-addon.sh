6e3bb8cba637da6e1bd15d41f783fa2175eada28
ohexus <moskalenko.taras.d@gmail.com>
2022-08-04 20:05:19 +0300

    fixed upload-addon script and workflows

--- upload-addon.sh
+++ upload-addon.sh
@@ -6,4 +6,4 @@ git archive --output=./$ZIP_NAME --format=zip HEAD EsoUI gamedata UkrainianScrol
 
 curl -X POST -H "x-api-token:$ESOUI_API_TOKEN" \
     -F id=$ESOUI_ID -F version=$VERSION -F compatible=8.0.0 -F updatefile=@/home/runner/work/EsoUA/EsoUA/$ZIP_NAME \
-    https://api.esoui.com/addons/updatetest
+    https://api.esoui.com/addons/update
