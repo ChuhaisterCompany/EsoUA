fe3367c8fd9d0707aa6e688f27aa5b8473755d57
ohexus <moskalenko.taras.d@gmail.com>
2022-08-04 18:42:24 +0300

    test workflow v6

--- upload-addon.sh
+++ upload-addon.sh
@@ -3,10 +3,12 @@ ID=3437
 VERSION=$(cat .VERSION)
 ZIP_NAME=EsoUA$VERSION.zip
 
-git archive --output=./$ZIP_NAME --format=zip HEAD EsoUI gamedata UkrainianScrollsOnline
+# git archive --output=./$ZIP_NAME --format=zip HEAD EsoUI gamedata UkrainianScrollsOnline
+
+cd
 
 ls -a
 
-curl -X POST -H "x-api-token:$ESOUI_API_TOKEN" \
-    -F id=$ID -F version=$VERSION -F compatible=8.0.0 -F updatefile=@/$ZIP_NAME \
-    https://api.esoui.com/addons/updatetest
+# curl -X POST -H "x-api-token:$ESOUI_API_TOKEN" \
+#     -F id=$ID -F version=$VERSION -F compatible=8.0.0 -F updatefile=@/$ZIP_NAME \
+#     https://api.esoui.com/addons/updatetest
