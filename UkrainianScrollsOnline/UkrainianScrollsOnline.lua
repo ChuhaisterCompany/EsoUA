5e48609ecfc2102c733dd4b1cd78d26379cf2ff6
Stanislav <108600378+w2unemdhysend@users.noreply.github.com>
2022-08-10 04:50:57 +0300

    Release 1.09 (#37)
    * Update ua_client.str
    
    * Update ua_client.str
    
    * Release_v0.0.7
    
    * Update ua_client.str
    
    * 1.03
    
    * Update ua_client.str
    
    * Update UkrainianScrollsOnline.lua
    
    * Update UkrainianScrollsOnline.txt
    
    * Release 1.05
    
    * Release 1.06
    
    * Release 1.07
    
    * Release 1.08
    
    * Release 1.09

--- UkrainianScrollsOnline/UkrainianScrollsOnline.lua
+++ UkrainianScrollsOnline/UkrainianScrollsOnline.lua
@@ -1,6 +1,6 @@
 local UkrainianScrollsOnline = {}
 UkrainianScrollsOnline.name  = "Ukrainian Scrolls Online"
-UkrainianScrollsOnline.version = "1.08"
+UkrainianScrollsOnline.version = "1.09"
 UkrainianScrollsOnline.langString = nil
 UkrainianScrollsOnline.positionning = false
 UkrainianScrollsOnline.Flags = { "en", "ua", "ut"}
