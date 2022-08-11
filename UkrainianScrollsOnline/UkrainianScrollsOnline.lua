3e63e57fe3a651d3328cd56210a786ceaa1cf1ac
Stanislav <108600378+w2unemdhysend@users.noreply.github.com>
2022-08-11 06:09:05 +0300

    Release 1.10

--- UkrainianScrollsOnline/UkrainianScrollsOnline.lua
+++ UkrainianScrollsOnline/UkrainianScrollsOnline.lua
@@ -1,6 +1,6 @@
 local UkrainianScrollsOnline = {}
 UkrainianScrollsOnline.name  = "Ukrainian Scrolls Online"
-UkrainianScrollsOnline.version = "1.09"
+UkrainianScrollsOnline.version = "1.10"
 UkrainianScrollsOnline.langString = nil
 UkrainianScrollsOnline.positionning = false
 UkrainianScrollsOnline.Flags = { "en", "ua", "ut"}
