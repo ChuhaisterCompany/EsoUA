1cf43714ebee2e1ca1d51756693e5bdc67442923
Stanislav <108600378+w2unemdhysend@users.noreply.github.com>
2022-08-29 02:47:27 +0300

    Release 1.23 (#63)
    * Update upload-addon.sh
    
    * Release 1.22
    
    * Release 1.23

--- UkrainianScrollsOnline/UkrainianScrollsOnline.lua
+++ UkrainianScrollsOnline/UkrainianScrollsOnline.lua
@@ -1,6 +1,6 @@
 local UkrainianScrollsOnline = {}
 UkrainianScrollsOnline.name  = "Ukrainian Scrolls Online"
-UkrainianScrollsOnline.version = "1.22"
+UkrainianScrollsOnline.version = "1.23"
 UkrainianScrollsOnline.langString = nil
 UkrainianScrollsOnline.positionning = false
 UkrainianScrollsOnline.Flags = { "en", "ua", "ut"}
