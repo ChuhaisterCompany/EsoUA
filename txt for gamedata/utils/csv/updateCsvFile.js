5178f27a3986ba610495b112b4431fd8108a56fc
ohexus <moskalenko.taras.d@gmail.com>
2023-02-01 15:51:06 +0200

    Update path for updateCsvFile script

--- txt for gamedata/utils/csv/updateCsvFile.js
+++ txt for gamedata/utils/csv/updateCsvFile.js
@@ -3,36 +3,30 @@ const path = require('path');
 
 const getCsvTable = require('./getCsvTable');
 
-fs.readFile(
-  path.resolve(__dirname, '..', '..', 'csv', 'en.lang.csv'),
-  { encoding: 'utf8' },
-  (err, data) => {
+const FILE_PATH = path.resolve(__dirname, '../../csv', 'en.lang.csv');
+
+fs.readFile(FILE_PATH, { encoding: 'utf8' }, (err, data) => {
+  if (err) {
+    console.error(err.message);
+    return;
+  }
+
+  const dataObj = data.split(/\r?\n/).reduce((keys, line, index) => {
+    if (index === 0 || !line.length) {
+      return keys;
+    }
+
+    const [key, value] = line.split(',');
+
+    keys[key] = value;
+
+    return keys;
+  }, {});
+
+  fs.writeFile(FILE_PATH, getCsvTable(Object.entries(dataObj)), (err) => {
     if (err) {
       console.error(err.message);
       return;
     }
-
-    const dataObj = data.split(/\r?\n/).reduce((keys, line, index) => {
-      if (index === 0 || !line.length) {
-        return keys;
-      }
-
-      const [key, value] = line.split(',');
-
-      keys[key] = value;
-
-      return keys;
-    }, {});
-
-    fs.writeFile(
-      path.resolve(__dirname, '..', '..', 'csv', 'en-updated.lang.csv'),
-      getCsvTable(Object.entries(dataObj)),
-      (err) => {
-        if (err) {
-          console.error(err.message);
-          return;
-        }
-      },
-    );
-  },
-);
+  });
+});
