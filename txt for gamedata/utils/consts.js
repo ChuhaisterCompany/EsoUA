71353bd79df93fc41c3891fc8fef4b3cd03880c6
ohexus <moskalenko.taras.d@gmail.com>
2023-03-05 15:11:23 +0200

    Fix regexp v4.1

--- txt for gamedata/utils/consts.js
+++ txt for gamedata/utils/consts.js
@@ -5,7 +5,7 @@ const BASE_DIR = path.resolve(__dirname, '..');
 // const FILE_PATH = path.resolve(BASE_DIR, '../update-36/ut.lang.txt');
 const FILE_PATH = path.resolve(__dirname, '../csv/en.lang.csv');
 
-const CSV_BASE_PATTERN = `"(?<key>\\d+-\\d+-\\d+)"(,|;)"(?<value>([\\w\\s\\n$&+,:;=?@#|'<>.^*\\-()%!"])*?)"`;
+const CSV_BASE_PATTERN = `"(?<key>\\d+-\\d+-\\d+)"(,|;)"(?<value>([\\w\\W\\s\\n$&+,:;=?@#|'<>.^*\\-()%!"])*?)"`;
 const CSV_KEY_PATTERN = new RegExp(CSV_BASE_PATTERN);
 const CSV_PATTERN_FOR_UPDATE = new RegExp(CSV_BASE_PATTERN + '(,|;)""');
 
