300636ad3bbf6ca1a73fb8ef095752afacadfea4
ohexus <moskalenko.taras.d@gmail.com>
2023-03-05 15:01:34 +0200

    Fix regexp pattern for CSV parser v4

--- txt for gamedata/utils/consts.js
+++ txt for gamedata/utils/consts.js
@@ -5,14 +5,13 @@ const BASE_DIR = path.resolve(__dirname, '..');
 // const FILE_PATH = path.resolve(BASE_DIR, '../update-36/ut.lang.txt');
 const FILE_PATH = path.resolve(__dirname, '../csv/en.lang.csv');
 
-const BASE_PATTERN = /"(?<key>\d+-\d+-\d+)"(,|;)"(?<value>([\w\s\\n$&+,:;=?@#|'<>.^*\-()%!"])*?)"/;
-// const PATTERN_FOR_UPDATE_0 =
-//   /"(?<key>\d+-\d+-\d+)"(,|;)"(?<value>([\w\s\\n$&+,:;=?@#|'<>.^*\-()%!"])*?)"(,|;)""/;
-const PATTERN_FOR_UPDATE = new RegExp(BASE_PATTERN + '(,|;)""');
+const CSV_BASE_PATTERN = `"(?<key>\\d+-\\d+-\\d+)"(,|;)"(?<value>([\\w\\s\\n$&+,:;=?@#|'<>.^*\\-()%!"])*?)"`;
+const CSV_KEY_PATTERN = new RegExp(CSV_BASE_PATTERN);
+const CSV_PATTERN_FOR_UPDATE = new RegExp(CSV_BASE_PATTERN + '(,|;)""');
 
 const CSV = {
-  patternForUpdate: PATTERN_FOR_UPDATE,
-  keyPattern: BASE_PATTERN,
+  patternForUpdate: CSV_PATTERN_FOR_UPDATE,
+  keyPattern: CSV_KEY_PATTERN,
   outputDir: 'csv_ids',
 };
 
--- txt for gamedata/utils/csv/updateCsvFile.js
+++ txt for gamedata/utils/csv/updateCsvFile.js
@@ -6,34 +6,52 @@ const getCsvTable = require('./getCsvTable');
 
 const FILE_PATH = path.resolve(__dirname, '../../csv', 'en.lang.csv');
 
+const writeCSV = (data) => {
+  fs.writeFile(FILE_PATH, data, (err) => {
+    if (err) {
+      console.error(err.message);
+      return;
+    }
+  });
+};
+
 fs.readFile(FILE_PATH, { encoding: 'utf8' }, (err, data) => {
   if (err) {
     console.error(err.message);
     return;
   }
 
-  const dataObj = data.split(/\r?\n/).reduce((keys, line, index) => {
-    if (index === 0 || !line.length) {
-      return keys;
+  const dataObj = {};
+
+  data.split(/\r?\n/).every((line, index) => {
+    if (!line.length) {
+      return true;
+    }
+
+    if (index === 0) {
+      const lineArr = line.split(/[;,]/);
+
+      if (lineArr.length <= 2) {
+        return false;
+      }
+
+      return true;
     }
 
     const groups = line.match(CSV.patternForUpdate)?.groups;
 
     if (!groups) {
-      return keys;
+      return false;
     }
 
     const { key, value } = groups;
 
-    keys[key] = value || '';
+    dataObj[key] = value || '';
 
-    return keys;
-  }, {});
-
-  fs.writeFile(FILE_PATH, getCsvTable(Object.entries(dataObj)), (err) => {
-    if (err) {
-      console.error(err.message);
-      return;
-    }
+    return true;
   });
+
+  const dataEntries = Object.entries(dataObj);
+
+  writeCSV(dataEntries.length ? getCsvTable(dataEntries) : data);
 });
